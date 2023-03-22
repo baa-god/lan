49fe3a4990beb315ed373a2c1a790b077808a951
黄江桂 <780810441@qq.com>
2023-03-22 10:10:43 +0800

    release v0.0.1

--- sharp.go
+++ sharp.go
@@ -1,9 +1,22 @@
 package sharp
 
 import (
+	"crypto/rand"
+	"crypto/sha256"
+	"fmt"
 	"io"
+	"math/big"
 	"os"
+	"strconv"
 	"strings"
+)
+
+const (
+	Digits         = "0123456789"
+	AsciiLowercase = "abcdefghijklmnopqrstuvwxyz"
+	AsciiUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	Punctuation    = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+	Asciis         = Digits + AsciiLowercase + AsciiUppercase + Punctuation
 )
 
 // ReadLastLine 读取 file 最后一行文本
@@ -28,4 +41,29 @@ func ReadLastLine(file *os.File) (line string, err error) {
 	}
 
 	return
+}
+
+func Rand(n int) (s string) {
+	length := len(Asciis)
+	for i := 0; i < n; i++ {
+		// bigI: [0, length-1]
+		bigI, _ := rand.Int(rand.Reader, big.NewInt(int64(length)))
+		index, _ := strconv.Atoi(bigI.String())
+		s += fmt.Sprintf("%c", Asciis[index])
+	}
+	return
+}
+
+func Sha256(text string) string {
+	sum := fmt.Sprintf("%x", sha256.Sum256([]byte(text)))
+	return strings.ToUpper(sum)
+}
+
+func HasSuffix(s string, a ...string) bool {
+	for _, v := range a {
+		if strings.HasSuffix(s, v) {
+			return true
+		}
+	}
+	return false
 }
