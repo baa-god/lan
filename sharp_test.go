b885351244b1e609e09fb1b84091a0eda63d38e0
黄江桂 <780810441@qq.com>
2023-03-22 14:56:32 +0800

    update

--- sharp_test.go
+++ sharp_test.go
@@ -1,15 +1,7 @@
 package sharp
 
 import (
-	"fmt"
-	"github.com/baa-god/sharp/ip2region"
 	"testing"
 )
 
-func TestFunc(t *testing.T) {
-	err := ip2region.InitIP2Region("ip2region.xdb")
-	fmt.Println("err:", err)
-
-	r := ip2region.MustSearch("192.168.1.241")
-	fmt.Printf("r: %+v\n", r)
-}
+func TestFunc(t *testing.T) {}
