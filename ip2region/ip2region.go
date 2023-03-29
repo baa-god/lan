ef1c5d9f705a35f54c2687c7ed4de0e4594ac7e9
黄江桂 <780810441@qq.com>
2023-03-29 11:22:49 +0800

    update

--- ip2region/ip2region.go
+++ ip2region/ip2region.go
@@ -47,19 +47,15 @@ func IsIntranet(ip string) bool {
 	return MustSearch(ip).City == "内网IP"
 }
 
-func InitIP2Region(dbPath string) (err error) {
-	// 从 dbPath 加载 VectorIndex 缓存，把下述 vIndex 变量全局到内存里面。
+func InitIP2Region(dbPath string) {
 	vIndex, err := xdb.LoadVectorIndexFromFile(dbPath)
 	if err != nil {
-		fmt.Printf("failed to load vector index from `%s`: %s\n", dbPath, err)
-		return
+		panic(err)
 	}
 
-	// 用全局的 vIndex 创建带 VectorIndex 缓存的查询对象
 	searcher, err = xdb.NewWithVectorIndex(dbPath, vIndex)
 	if err != nil {
-		fmt.Printf("failed to create searcher with vector index: %s\n", err)
-		return
+		panic(err)
 	}
 
 	return
