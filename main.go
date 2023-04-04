988848b1758a9233d0423e83ce0f5a90e9deffeb
黄江桂 <780810441@qq.com>
2023-04-04 18:44:14 +0800

    update

--- main.go
+++ main.go
@@ -1,16 +1,5 @@
 package main
 
-import (
-	"github.com/baa-god/lan/wood"
-	"os"
-)
-
 func main() {
-	wood.Trace("xxx")
 
-	l := wood.New(os.Stdout)
-	l.Debug("ssss1")
-
-	wood.Debug("xxx")
-	l.Info("xxxx1")
 }
