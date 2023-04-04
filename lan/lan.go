89c8b3081a42d009ac31f1784dc695a502bb18bb
黄江桂 <780810441@qq.com>
2023-04-04 14:25:12 +0800

    update

--- lan/lan.go
+++ lan/lan.go
@@ -42,7 +42,7 @@ func If[T any](b bool, v T, e T) T {
 	return e
 }
 
-func Copy[M1 ~map[K]V, M2 ~map[K]V, K comparable, V any](dst M1, src M2) M1 {
+func CopyMap[M1 ~map[K]V, M2 ~map[K]V, K comparable, V any](dst M1, src M2) M1 {
 	for k, v := range src {
 		dst[k] = v
 	}
