1673eb26db49286c4b40b44b8c05d255aced67fa
黄江桂 <780810441@qq.com>
2023-04-04 14:24:19 +0800

    update

--- lan/lan.go
+++ lan/lan.go
@@ -42,6 +42,13 @@ func If[T any](b bool, v T, e T) T {
 	return e
 }
 
+func Copy[M1 ~map[K]V, M2 ~map[K]V, K comparable, V any](dst M1, src M2) M1 {
+	for k, v := range src {
+		dst[k] = v
+	}
+	return dst
+}
+
 func Unmarshal(data any, output any) (err error) {
 	if v, err := json.Marshal(data); err == nil {
 		err = json.Unmarshal(v, output)
