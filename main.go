40b73fd0c0732d64fe0e5258c59c0e29c9b08fc3
BaaGod <baagod@proton.me>
2023-06-03 04:37:27 +0800

    添加一个 Abort() 方法

--- main.go
+++ main.go
@@ -1,26 +1,3 @@
 package main
 
-import (
-	"fmt"
-	"github.com/baa-god/lan/fib"
-	"github.com/gofiber/fiber/v2"
-)
-
-func main() {
-	r := fib.New()
-	r.Post("test/post", func(c *fib.Ctx) {
-		fmt.Println(c.Arg("name"))
-		fmt.Println(c.Get("name"))
-	})
-
-	r.Get("test/get", func(c *fib.Ctx) {
-		c.Route().Handlers = []fiber.Handler{}
-		fmt.Println("================")
-	}, func(c *fib.Ctx) (err error) {
-		fmt.Println("argName:", c.Query("name"))
-		err = c.SendString("6666")
-		return err
-	})
-
-	r.Listen(":800")
-}
+func main() {}
